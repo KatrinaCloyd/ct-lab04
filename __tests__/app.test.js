5be39a0efc08d4f33cfff82b854ba921bea580c3
Katrina Cloyd <Katrina.cloyd@yahoo.com>
2021-03-26 15:58:50 -0700

    got advise back from api and that part of test passing, now confused by texts...

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,22 +2,25 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const { sendSms } = require('../lib/utils/twilio');
 
 describe('ct-lab04 routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
+  beforeEach(() => {
+  });
+
   it('creates a new user with auto generated advise', async () => {
     const res = await request(app)
       .post('/api/v1/useradvise')
       .send({ userName: 'Bob', birthMonth: 'March' });
-
     expect(res.body).toEqual({
-      id: expect.any(Number),
-      userName: expect.any(String),
-      birthMonth: expect.any(String),
-      advise: 'blah',
+      id: expect.any(String),
+      user_name: expect.any(String),
+      birth_month: expect.any(String),
+      advise: expect.any(String),
     });
 
   });
