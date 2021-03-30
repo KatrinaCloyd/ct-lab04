b29a4bca94bd882a2ef29dd6339c8c49da4ccd19
Katrina Cloyd <Katrina.cloyd@yahoo.com>
2021-03-30 11:56:04 -0700

    five routes written with RGR-TDD and all passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
 jest.mock('../lib/utils/twilio');
 const twilio = require('../lib/utils/twilio');
@@ -14,6 +15,7 @@ jest.mock('twilio', () => () => ({
 
 describe('ct-lab04 routes', () => {
   beforeEach(() => {
+    twilio.sendSms.mockReset();
     return setup(pool);
   });
 
@@ -28,6 +30,62 @@ describe('ct-lab04 routes', () => {
       birthMonth: expect.any(String),
       advice: expect.any(String),
     });
-
   });
+
+  it('gets all users', async () => {
+    await UserService.create({ userName: 'Bob', birthMonth: 'March' });
+    const res = await request(app)
+      .get('/api/v1/useradvice');
+    expect(twilio.sendSms).toHaveBeenCalledTimes(1);
+    expect(res.body).toEqual([{
+      id: expect.any(String),
+      userName: expect.any(String),
+      birthMonth: expect.any(String),
+      advice: expect.any(String),
+    }]);
+  });
+
+  it('gets user by id', async () => {
+    await UserService.create({ userName: 'Bob', birthMonth: 'March' });
+    const res = await request(app)
+      .get('/api/v1/useradvice/1');
+    expect(res.body).toEqual({
+      id: '1',
+      userName: 'Bob',
+      birthMonth: 'March',
+      advice: expect.any(String),
+    });
+  });
+
+  it('deletes a user by id', async () => {
+    await UserService.create({ userName: 'Bob', birthMonth: 'March' });
+    const res = await request(app)
+      .delete('/api/v1/useradvice/1');
+    expect(res.body).toEqual({
+      id: '1',
+      userName: 'Bob',
+      birthMonth: 'March',
+      advice: expect.any(String),
+    });
+  });
+
+  it('updates given users by id with new random advice', async () => {
+    const firstUser = await UserService.create({ userName: 'Bob', birthMonth: 'March' });
+    const res = await request(app)
+      .put('/api/v1/useradvice/1');
+
+    const isMatch = res.body.advice === firstUser.advice;
+    console.log(res.body.advice);
+    console.log(firstUser.advice);
+
+    expect(res.body).toEqual({
+      id: '1',
+      userName: 'Bob',
+      birthMonth: 'March',
+      advice: expect.any(String),
+    });
+    expect(isMatch).toEqual(false);
+    expect(twilio.sendSms).toHaveBeenCalledTimes(2);
+  });
+
 });
